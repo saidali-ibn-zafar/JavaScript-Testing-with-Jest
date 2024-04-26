c6fb779ff9c961281680ead25ff4ffd6f150bf42
saidali-ibn-zafar <zikirillayevsaidali18@gmail.com>
2024-04-26 16:38:58 -0300

    more examples have been done ...

--- sum.test.js
+++ sum.test.js
@@ -3,6 +3,37 @@
 //     expect(sum(1, 2)).toBe(3);
 // });
 
-test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
+// test('two plus two is four', () => {
+//     expect(2 + 2).toBe(4);
+// })
+
+// test("object assignment", () => {
+//     const data = {one: 1};
+//     data["two"] = 2
+//     expect(data).toEqual({one: 1, two: 2});
+// })
+
+
+// test("null is falsy", () => {
+//     const n = null;
+//     expect(n).toBeFalsy();
+// })
+
+// test("zero is falsy", () => {
+//     const n = 0;
+//     expect(n).toBeFalsy();
+// })
+
+// test("one is falsy", () => {
+//     const n = 1;
+//     expect(n).toBeTruthy();
+// })
+
+
+const myFunction = require("./sum")
+
+test("throws on invalid inputs", () => {
+    expect(()=>{
+        myFunction("qwerty");
+    }).toThrow();
 })
